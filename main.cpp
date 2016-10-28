f27fe6c66ddafdd9a1a357fc0501869ea933ecd1
Daniel Slone <slonej3@nku.edu>
2016-10-28 11:30:48 -0400

    priority queue is working

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include "Patient.h"
 #include "Utilities.h"
 using namespace std;
-
+struct Priority {
+    bool operator()(Patient const & p1, Patient const & p2) {
+        // return "true" if "p1" is ordered before "p2", for example:
+        return p1.getPriority() > p2.getPriority();
+    }
+};
 int main() {
     vector<Patient> patients;
     Utilities::read(&patients);
+    std::priority_queue<Patient, std::vector<Patient>, Priority> pq;
+    for (int i = 0; i < patients.size(); ++i) {
+        pq.push(patients.at(i));
+    }
+    while (!pq.empty()) {
+        Patient patient = pq.top();
+        cout << patient.getPriority() << endl;
+        pq.pop();
+    }
     return 0;
 }
